b5f594feb11b875dc1f50b5f4d45abc998f65a2d
Sumuyzzz <Sumuyzzz@gmail.com>
2021-06-23 22:37:59 +0800

    deploy

---
+++ precache-manifest.a770fba775516c4a0f58fede8ea4cbb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ecbfcf34d65c076f83d",
+    "url": "/money-vue2_website/css/app.a9b3baab.css"
+  },
+  {
+    "revision": "ce27ce3475f771aab20b3bb1d7661a57",
+    "url": "/money-vue2_website/index.html"
+  },
+  {
+    "revision": "3ecbfcf34d65c076f83d",
+    "url": "/money-vue2_website/js/app.001afc20.js"
+  },
+  {
+    "revision": "60104d57467c9b4c2efb",
+    "url": "/money-vue2_website/js/chunk-vendors.820ec289.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/money-vue2_website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-vue2_website/robots.txt"
+  }
+]);
